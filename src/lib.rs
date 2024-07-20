09e60bea69e54883470c86f59938955507477844
tree1109 <55215480+tree1109@users.noreply.github.com>
2024-07-20 15:29:29 +0800

    feat: add Time test

--- src/lib.rs
+++ src/lib.rs
@@ -116,3 +116,54 @@ impl TimeLog {
             .unwrap()
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_new() {
+        let time = Time::new(12, 5).unwrap();
+        assert_eq!(*time.get_hours(), 12);
+        assert_eq!(*time.get_minutes(), 5);
+        let time = Time::new(24, 2);
+        assert!(time.is_none());
+        let time = Time::new(12, 68);
+        assert!(time.is_none());
+        let time = Time::new(25, 99);
+        assert!(time.is_none());
+    }
+
+    #[test]
+    fn test_total_minutes() {
+        let time = Time::new(12, 5).unwrap();
+        assert_eq!(time.total_minutes(), 725);
+        let time = Time::new(16, 48).unwrap();
+        assert_eq!(time.total_minutes(), 1008);
+        let time = Time::new(12, 30).unwrap();
+        assert_eq!(time.total_minutes(), 750);
+    }
+
+    #[test]
+    fn test_get_difference_in_minutes() {
+        let start_time = Time::new(12, 5).unwrap();
+        let end_time = Time::new(13, 10).unwrap();
+        assert_eq!(start_time.get_difference_in_minutes(&end_time), 65);
+        let start_time = Time::new(12, 5).unwrap();
+        let end_time = Time::new(11, 10).unwrap();
+        assert_eq!(start_time.get_difference_in_minutes(&end_time), 1385);
+        let start_time = Time::new(12, 5).unwrap();
+        let end_time = Time::new(12, 5).unwrap();
+        assert_eq!(start_time.get_difference_in_minutes(&end_time), 0);
+    }
+
+    #[test]
+    fn test_get_time_string() {
+        let time = Time::new(12, 5).unwrap();
+        assert_eq!(time.get_time_string(), "12:05");
+        let time = Time::new(16, 48).unwrap();
+        assert_eq!(time.get_time_string(), "16:48");
+        let time = Time::new(3, 3).unwrap();
+        assert_eq!(time.get_time_string(), "03:03");
+    }
+}
