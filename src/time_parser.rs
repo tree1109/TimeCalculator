f268454859b1a099ebac414ed7b165015137ea47
tree1109 <55215480+tree1109@users.noreply.github.com>
2024-07-20 17:17:42 +0800

    feat: add time parser tests and modify

--- src/time_parser.rs
+++ src/time_parser.rs
@@ -1,15 +1,15 @@
-use super::Time;
-use super::TimeInterval;
+use crate::Time;
+use crate::TimeInterval;
 use regex::Regex;
 
-fn get_time_from_string(time: &str) -> Time {
+pub fn get_time_from_string(time: &str) -> Time {
     let time: Vec<&str> = time.split(":").collect();
     let hours: u16 = time[0].parse::<u16>().unwrap();
     let minutes: u16 = time[1].parse::<u16>().unwrap();
     Time::new(hours, minutes).unwrap()
 }
 
-fn get_time_interval_from_line(line: &str) -> TimeInterval {
+pub fn get_time_interval_from_line(line: &str) -> TimeInterval {
     let pattern: Regex = Regex::new(r"\d{1,2}:\d{2}").unwrap();
     let times: Vec<&str> = pattern.find_iter(line).map(|x| x.as_str()).collect();
     let start_time: Time = get_time_from_string(times[0]);
@@ -17,11 +17,19 @@ fn get_time_interval_from_line(line: &str) -> TimeInterval {
     TimeInterval::new(start_time, end_time)
 }
 
+pub fn get_time_interval_from_lines(lines: Vec<&str>) -> Vec<TimeInterval> {
+    lines
+        .iter()
+        .map(|x| get_time_interval_from_line(x))
+        .collect()
+}
+
 #[cfg(test)]
 mod tests {
-    use super::super::Time;
-    use super::super::TimeInterval;
     use super::*;
+    use crate::Time;
+    use crate::TimeInterval;
+    use crate::TimeLog;
 
     #[test]
     fn test_get_time_from_string() {
@@ -56,5 +64,26 @@ mod tests {
         assert_eq!(*time_interval.get_start_time().get_minutes(), 44);
         assert_eq!(*time_interval.get_end_time().get_hours(), 12);
         assert_eq!(*time_interval.get_end_time().get_minutes(), 34);
+    }
+
+    #[test]
+    fn test_get_time_intervals_from_lines() {
+        let lines: Vec<&str> = vec!["1. From 12:10 to 15:10", "2. From 6:00 to 8:05"];
+        let time_intervals: Vec<TimeInterval> = get_time_interval_from_lines(lines);
+        assert_eq!(time_intervals.len(), 2);
+        let time_log: TimeLog = TimeLog::new(time_intervals);
+        assert_eq!(time_log.get_total_time_in_minutes(), 305);
+        assert_eq!(time_log.get_total_time_string(), "05:05");
+        let lines: Vec<&str> = vec![
+            "1. From 1:10 to 6:10",
+            "2. From 12:00 to 0:05",
+            "3. From 12:40 to 13:40",
+            "4. From 8:50 to 23:50",
+        ];
+        let time_intervals: Vec<TimeInterval> = get_time_interval_from_lines(lines);
+        assert_eq!(time_intervals.len(), 4);
+        let time_log: TimeLog = TimeLog::new(time_intervals);
+        assert_eq!(time_log.get_total_time_in_minutes(), 1985);
+        assert_eq!(time_log.get_total_time_string(), "33:05");
     }
 }
