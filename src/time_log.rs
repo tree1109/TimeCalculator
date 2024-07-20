1e3eadde658ecfb50b3259e306290f84740aca94
tree1109 <55215480+tree1109@users.noreply.github.com>
2024-07-20 16:09:47 +0800

    feat: add TimeLog tests and fix get_time_log_strings()

--- src/time_log.rs
+++ src/time_log.rs
@@ -23,13 +23,49 @@ impl TimeLog {
     }
 
     pub fn get_time_log_strings(&self) -> String {
-        self.time_intervals
+        let strings: Vec<String> = self
+            .time_intervals
             .iter()
             .map(|x| x.get_time_interval_string())
-            .reduce(|mut acc, x| {
-                acc.push_str(&x);
-                acc
-            })
-            .unwrap()
+            .collect();
+        strings.join("\n")
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::super::time::Time;
+    use super::*;
+
+    fn get_time_intervals() -> Vec<TimeInterval> {
+        vec![
+            TimeInterval::new(Time::new(6, 0).unwrap(), Time::new(18, 30).unwrap()),
+            TimeInterval::new(Time::new(8, 0).unwrap(), Time::new(12, 0).unwrap()),
+            TimeInterval::new(Time::new(13, 0).unwrap(), Time::new(2, 0).unwrap()),
+        ]
+    }
+
+    #[test]
+    fn test_get_total_time_in_minutes() {
+        let time_intervals = get_time_intervals();
+        let time_log = TimeLog::new(time_intervals);
+        assert_eq!(time_log.get_total_time_in_minutes(), 1770);
+    }
+
+    #[test]
+    fn test_get_total_time_string() {
+        let time_intervals = get_time_intervals();
+        let time_log = TimeLog::new(time_intervals);
+        assert_eq!(time_log.get_total_time_string(), "29:30");
+    }
+
+    #[test]
+    fn test_get_time_log_strings() {
+        let time_intervals = get_time_intervals();
+        let time_log = TimeLog::new(time_intervals);
+        assert_eq!(
+            time_log.get_time_log_strings(),
+            "06:00 ~ 18:30\n08:00 ~ 12:00\n13:00 ~ 02:00"
+        );
     }
 }
