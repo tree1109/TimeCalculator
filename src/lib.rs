bfbd3e07103dba0f8103ea944deef09b5d4d2fd9
tree1109 <55215480+tree1109@users.noreply.github.com>
2024-07-20 21:26:24 +0800

    refactor: move run() and Config to lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -6,3 +6,40 @@ pub mod time_parser;
 pub use time::Time;
 pub use time_interval::TimeInterval;
 pub use time_log::TimeLog;
+
+use std::{error::Error, fs};
+
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    // Read the file
+    let contents = fs::read_to_string(config.file_path)?;
+
+    println!("檔案內容:\n{}", contents);
+
+    Ok(())
+}
+
+pub struct Config {
+    file_path: String,
+}
+
+impl Config {
+    pub fn new(args: &[String]) -> Result<Config, &'static str> {
+        match args.len() {
+            1 => {
+                println!("請指定要讀取的文字檔案，可將檔案拖曳到視窗\n檔案路徑:");
+                // get input string from user
+                let mut buf = String::new();
+                match std::io::stdin().read_line(&mut buf) {
+                    Ok(_) => Ok(Config {
+                        file_path: buf.trim().to_string(),
+                    }),
+                    Err(_) => Err("讀取輸入錯誤"),
+                }
+            }
+            2 => Ok(Config {
+                file_path: args[1].clone(),
+            }),
+            _ => Err("只能指定一個檔案路徑喔"),
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
-use std::{env, error::Error, fs, process};
+use std::{env, process};
+
+use summary_time_calculator::{run, Config};
+
 fn main() {
     // Get the command line arguments
     let args: Vec<String> = env::args().collect();
@@ -12,40 +15,5 @@ fn main() {
     if let Err(e) = run(config) {
         println!("執行發生錯誤: {}", e);
         process::exit(2);
-    }
-}
-
-fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    // Read the file
-    let contents = fs::read_to_string(config.file_path)?;
-
-    println!("檔案內容:\n{}", contents);
-
-    Ok(())
-}
-
-struct Config {
-    file_path: String,
-}
-
-impl Config {
-    fn new(args: &[String]) -> Result<Config, &'static str> {
-        match args.len() {
-            1 => {
-                println!("請指定要讀取的文字檔案，可將檔案拖曳到視窗\n檔案路徑:");
-                // get input string from user
-                let mut buf = String::new();
-                match std::io::stdin().read_line(&mut buf) {
-                    Ok(_) => Ok(Config {
-                        file_path: buf.trim().to_string(),
-                    }),
-                    Err(_) => Err("讀取輸入錯誤"),
-                }
-            }
-            2 => Ok(Config {
-                file_path: args[1].clone(),
-            }),
-            _ => Err("只能指定一個檔案路徑喔"),
-        }
     }
 }
