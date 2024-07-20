9043a81f9a208f57923ba115d2f80ac8ec2e9a59
tree1109 <55215480+tree1109@users.noreply.github.com>
2024-07-20 21:22:25 +0800

    refactor: improve file reading and error handling for command line argument and file reading

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{env, fs, process};
+use std::{env, error::Error, fs, process};
 fn main() {
     // Get the command line arguments
     let args: Vec<String> = env::args().collect();
@@ -9,18 +9,19 @@ fn main() {
         process::exit(1);
     });
 
-    // Run the program
-    run(config);
+    if let Err(e) = run(config) {
+        println!("執行發生錯誤: {}", e);
+        process::exit(2);
+    }
 }
 
-fn run(config: Config) {
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
     // Read the file
-    let time_log = fs::read_to_string(config.file_path).unwrap_or_else(|err| {
-        println!("讀取檔案時發生錯誤: {}", err);
-        process::exit(2);
-    });
+    let contents = fs::read_to_string(config.file_path)?;
 
-    println!("檔案內容:\n{}", time_log);
+    println!("檔案內容:\n{}", contents);
+
+    Ok(())
 }
 
 struct Config {
@@ -34,13 +35,17 @@ impl Config {
                 println!("請指定要讀取的文字檔案，可將檔案拖曳到視窗\n檔案路徑:");
                 // get input string from user
                 let mut buf = String::new();
-                std::io::stdin().read_line(&mut buf).unwrap();
-                Ok(Config { file_path: buf })
+                match std::io::stdin().read_line(&mut buf) {
+                    Ok(_) => Ok(Config {
+                        file_path: buf.trim().to_string(),
+                    }),
+                    Err(_) => Err("讀取輸入錯誤"),
+                }
             }
             2 => Ok(Config {
                 file_path: args[1].clone(),
             }),
-            _ => Err("只能指定一個檔案路徑喔!"),
+            _ => Err("只能指定一個檔案路徑喔"),
         }
     }
 }
