66bccd8140f1dd8bdb8aa66cf229ef5393cfc8f6
tree1109 <55215480+tree1109@users.noreply.github.com>
2024-07-20 22:06:38 +0800

    refactor: modify print message

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,7 @@ pub mod time_interval;
 pub mod time_log;
 pub mod time_parser;
 
+// Re-export the modules
 pub use time::Time;
 pub use time_interval::TimeInterval;
 pub use time_log::TimeLog;
@@ -13,14 +14,17 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     // Read the file
     let contents = fs::read_to_string(config.file_path)?;
 
+    // Parse the time intervals
     let time_intervals = time_parser::get_time_interval_from_lines(contents.lines().collect());
     let time_log = TimeLog::new(time_intervals);
 
-    let total_time = time_log.get_total_time_string();
+    // Print the time log and the total time
     let time_log_string = time_log.get_time_log_strings();
-
-    println!("總共時間:\n{}", total_time);
+    let total_time = time_log.get_total_time_in_minutes();
+    let hours = total_time / 60;
+    let minutes = total_time % 60;
     println!("時間紀錄:\n{}", time_log_string);
+    println!("總共時間:\n總共{}小時又{}分鐘~ UwU", hours, minutes);
 
     Ok(())
 }
@@ -32,12 +36,15 @@ pub struct Config {
 impl Config {
     pub fn new(args: &[String]) -> Result<Config, &'static str> {
         match args.len() {
+            // If there is no file path, use the default file path
             1 => Ok(Config {
                 file_path: String::from("time_log.txt"),
             }),
+            // If there is a file path, use the file path from the command line arguments
             2 => Ok(Config {
                 file_path: args[1].clone(),
             }),
+            // If there are more than one file path, return an error
             _ => Err("只能指定一個檔案路徑喔"),
         }
     }
