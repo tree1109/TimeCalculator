86f4193375124a7f8a13d73e2bb522d532859739
tree1109 <55215480+tree1109@users.noreply.github.com>
2024-07-20 14:49:10 +0800

    feat: add TimeInterval struct

--- src/lib.rs
+++ src/lib.rs
@@ -36,7 +36,7 @@ impl Time {
         self.hours * 60 + self.minutes
     }
 
-    fn get_difference_in_minutes(&self, end_time: Self) -> u16 {
+    fn get_difference_in_minutes(&self, end_time: &Self) -> u16 {
         let mut ent_time_minutes = end_time.total_minutes();
         if (end_time.get_hours() < &self.hours) {
             ent_time_minutes += 24 * 60;
@@ -46,5 +46,39 @@ impl Time {
 
     fn get_time_string(&self) -> String {
         format!("{:02}:{:02}", self.hours, self.minutes)
+    }
+}
+
+struct TimeInterval {
+    start_time: Time,
+    end_time: Time,
+}
+
+impl TimeInterval {
+    fn new(start_time: Time, end_time: Time) -> Self {
+        Self {
+            start_time,
+            end_time,
+        }
+    }
+
+    fn get_start_time(&self) -> &Time {
+        &self.start_time
+    }
+
+    fn get_end_time(&self) -> &Time {
+        &self.end_time
+    }
+
+    fn get_time_interval_in_minutes(&self) -> u16 {
+        self.start_time.get_difference_in_minutes(&self.end_time)
+    }
+
+    fn get_time_interval_string(&self) -> String {
+        format!(
+            "{} ~ {}",
+            self.start_time.get_time_string(),
+            self.end_time.get_time_string(),
+        )
     }
 }
