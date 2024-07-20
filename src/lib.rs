68ceae9ea9436426decb86b67bd85072e76d0108
tree1109 <55215480+tree1109@users.noreply.github.com>
2024-07-20 15:16:29 +0800

    feat: add Timelog struct

--- src/lib.rs
+++ src/lib.rs
@@ -82,3 +82,37 @@ impl TimeInterval {
         )
     }
 }
+
+struct TimeLog {
+    time_intervals: Vec<TimeInterval>,
+}
+
+impl TimeLog {
+    fn new(time_intervals: Vec<TimeInterval>) -> Self {
+        Self { time_intervals }
+    }
+
+    fn get_total_time_in_minutes(&self) -> u16 {
+        self.time_intervals
+            .iter()
+            .fold(0, |acc, x| acc + x.get_time_interval_in_minutes())
+    }
+
+    fn get_total_time_string(&self) -> String {
+        let total = self.get_total_time_in_minutes();
+        let hours = total / 60;
+        let minutes = total % 60;
+        format!("{:02}:{:02}", hours, minutes)
+    }
+
+    fn get_time_log_strings(&self) -> String {
+        self.time_intervals
+            .iter()
+            .map(|x| x.get_time_interval_string())
+            .reduce(|mut acc, x| {
+                acc.push_str(&x);
+                acc
+            })
+            .unwrap()
+    }
+}
